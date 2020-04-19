fea43c0e05f530d2f9dde21a4a168f43aa606177
asmitadhungana <39458075+asmitadhungana@users.noreply.github.com>
2020-04-19 10:30:41 +0545

    set the route up for campaigns/requests  page

---
+++ pages/campaigns/requests/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class RequestsIndex extends Component {
+  state = {};
+  render() {
+    return <h3>reqindex</h3>;
+  }
+}
+
+export default RequestsIndex;
