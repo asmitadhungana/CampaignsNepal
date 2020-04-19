a21ebea107c4c7b00a8c18e0b66defc62ec38529
asmitadhungana <39458075+asmitadhungana@users.noreply.github.com>
2020-04-19 10:17:58 +0545

    finished ContributeForm

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -2,27 +2,38 @@ import React, { Component } from "react";
 import { Form, Input, Message, Button } from "semantic-ui-react";
 import Campaign from "../ethereum/campaign";
 import web3 from "../ethereum/web3";
+import { Router } from "../routes"; //Router object from routes.js
 
 class ContributeForm extends Component {
   state = {
     value: "",
+    errorMessage: "",
+    loading: false,
   };
 
   onSubmit = async (event) => {
     event.preventDefault();
     const campaign = Campaign(this.props.campaignAddress); //making the campaign instance of current campaign available inside our onSubmit fxn
+    this.setState({ loading: true, errorMessage: "" }); //turn the loading spinner on
 
+    //call the contribute() fxn in Cmapiagn contract from user's account
     try {
       const accounts = await web3.eth.getAccounts();
       await campaign.methods.contribute().send({
         from: accounts[0], //1st accnt in user's metamask
         value: web3.utils.toWei(this.state.value, "ether"),
       });
-    } catch (err) {}
+
+      Router.replaceRoute(`/campaigns/${this.props.campaignAddress}`); //re-route to the same page to refresh the page and get getInitialProps to re-run so that the states of the contributorsCount and campaignBalance get updated in CampaginShow
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+
+    this.setState({ loading: false, value: "" }); //turn the loading spinner off and re-set the value in the input box to empty string
   };
   render() {
     return (
-      <Form onSubmit={this.onSubmit}>
+      <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
         <Form.Field>
           <label>Amount to Contribute</label>
           <Input
@@ -32,7 +43,11 @@ class ContributeForm extends Component {
             labelPosition="right"
           />
         </Form.Field>
-        <Button primary>Contribute!</Button>
+
+        <Message error header="Oops!" content={this.state.errorMessage} />
+        <Button primary loading={this.state.loading}>
+          Contribute!
+        </Button>
       </Form>
     );
   }
